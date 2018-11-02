0557b99403914de49f1e92d8a66749a587a144bf
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2018-11-02 12:25:23 -0500

    feat: read in contents from html file and append to list

--- crawler.py
+++ crawler.py
@@ -4,7 +4,12 @@ import os
 
 #contents = urllib.request.urlopen('a.html', ).read()
 
-__location__ = os.path.realpath(os.path.join(
-    os.getcwd(), os.path.dirname('a.html')))
+#__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname('a.html')))
+file_one = "a.html"
 
-print(__location__)
+content = []
+with open(file_one, "rb") as f:
+    s = f.read()
+    content.append(s)
+
+print(content)
