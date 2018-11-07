ed29ffa72bffd8cf94ac021c322a14d557304add
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2018-11-06 22:15:52 -0600

    feat: complete working crawler again!

--- crawler_two.py
+++ crawler_two.py
@@ -6,7 +6,7 @@ def crawl_network(seed):
     crawled = []
     to_crawl.append(seed)
     while to_crawl:
-        extract_links(get_link_text(to_crawl.pop()), crawled, to_crawl, web)
+        extract_links(to_crawl.pop(), crawled, to_crawl, web)
     print('web is: ', web)
     return
 
@@ -16,36 +16,39 @@ def get_link_text(link):
     with open(link, 'r') as f:
         s = f.read()
         contents += s
-        if len(contents) > 10:
-            print('successfully read in a page')
+        # if len(contents) > 10:
+        #print('successfully read in a page')
     return contents
 
 
-def extract_links(link_text, crawled, to_crawl, web):
-    if link_text not in web:
-        web[link_text] = []
-    crawled.append(link_text)
-    linkins_page = 0
+def extract_links(linky, crawled, to_crawl, web):
+    link_text = get_link_text(linky)
+    if linky not in web:
+        web[linky] = []
+    crawled.append(linky)
+    linksin_page = 0
     link = True
+    page_contents = link_text
     while link:
-        page_contents = link_text
         start_link = page_contents.find(
             "\"", page_contents.find("<a href=")) + 1
         end_link = page_contents.find("\"", start_link + 1)
-        if start_link == 0:
+        if start_link < 1:
             link = None
         else:
             link = page_contents[start_link:end_link]
             page_contents = page_contents[end_link + 1:]
-            if link not in web[link_text]:
-                web[link_text].append(link)
+            if link not in web[linky]:
+                web[linky].append(link)
             if link not in crawled and link not in to_crawl:
                 to_crawl.append(link)
-            linkins_page += 1
-    print("finished searching for links in the page, there were: ", linkins_page)
+            linksin_page += 1
+    # print("finished searching for links in the page, there were: ", linksin_page)
+    # print('web: ', web)
+    # print('web for this link list: ', web[linky])
     return
 
 
-print('starting')
+# print('starting')
 crawl_network('a.html')
-print('done!')
+# print('done!')
